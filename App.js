0ee675932629a5709a4a9ccb10ca49f0e14559cf
Ridwan Balogun <balogunridwan@gmail.com>
2019-12-13 13:24:05 +0100

    Add audiobook playlist

--- App.js
+++ App.js
@@ -1,7 +1,57 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+
+const audioBookPlaylist = [
+  {
+    title: 'Hamlet - Act I',
+    author: 'William Shakespeare',
+    source: 'Librivox',
+    uri:
+      'https://ia800204.us.archive.org/11/items/hamlet_0911_librivox/hamlet_act1_shakespeare.mp3',
+    imageSource:
+      'http://www.archive.org/download/LibrivoxCdCoverArt8/hamlet_1104.jpg'
+  },
+  {
+    title: 'Hamlet - Act II',
+    author: 'William Shakespeare',
+    source: 'Librivox',
+    uri:
+      'https://ia600204.us.archive.org/11/items/hamlet_0911_librivox/hamlet_act2_shakespeare.mp3',
+    imageSource:
+      'http://www.archive.org/download/LibrivoxCdCoverArt8/hamlet_1104.jpg'
+  },
+  {
+    title: 'Hamlet - Act III',
+    author: 'William Shakespeare',
+    source: 'Librivox',
+    uri:
+      'http://www.archive.org/download/hamlet_0911_librivox/hamlet_act3_shakespeare.mp3',
+    imageSource:
+      'http://www.archive.org/download/LibrivoxCdCoverArt8/hamlet_1104.jpg'
+  },
+  {
+    title: 'Hamlet - Act IV',
+    author: 'William Shakespeare',
+    source: 'Librivox',
+    uri:
+      'https://ia800204.us.archive.org/11/items/hamlet_0911_librivox/hamlet_act4_shakespeare.mp3',
+    imageSource:
+      'http://www.archive.org/download/LibrivoxCdCoverArt8/hamlet_1104.jpg'
+  },
+  {
+    title: 'Hamlet - Act V',
+    author: 'William Shakespeare',
+    source: 'Librivox',
+    uri:
+      'https://ia600204.us.archive.org/11/items/hamlet_0911_librivox/hamlet_act5_shakespeare.mp3',
+    imageSource:
+      'http://www.archive.org/download/LibrivoxCdCoverArt8/hamlet_1104.jpg'
+  }
+]
+
 export default function App() {
+  console.log(audioBookPlaylist[0])
   return (
     <View style={styles.container}>
       <Text>Ridbay Audio Player</Text>
