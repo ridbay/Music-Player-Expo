9e7919f15aeae24b00539793ea9aa271493c7c74
Ridwan Balogun <balogunridwan@gmail.com>
2019-12-17 17:55:33 +0100

    Load the Audio file

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { StyleSheet, Text, View, TouchableOpacity, Image } from 'react-native';
-import {Ionicons} from '@expo/vector-icons';
-import {Audio} from 'expo-av';
+import { Ionicons } from '@expo/vector-icons';
+import { Audio } from 'expo-av';
 
 
 const audioBookPlaylist = [
@@ -53,60 +53,78 @@ const audioBookPlaylist = [
 ]
 
 export default class App extends React.Component {
- state = {
-   isPlaying: false,
-   playbackInstance: null,
-   currentIndex: 0,
-   volume: 1.0,
-   isBuffering: false,
- }
-
- async componentDidMount(){
-
-  try {
-    await Audio.setAudioModeAsync({
-      allowsRecordingIOS: false,
-      interruptionModeIOS: Audio.INTERRUPTION_MODE_IOS_DO_NOT_MIX,
-      playsInSilentModeIOS: true,
-      interruptionModeAndroid: Audio.INTERRUPTION_MODE_ANDROID_DUCK_OTHERS,
-      shouldDuckAndroid: true,
-      staysActiveInBackground: true,
-      playThroughEarpieceAndroid: true,
-    })
-    this.loadAudio()
-  } catch (e) {
-    console.log(e)
+  state = {
+    isPlaying: false,
+    playbackInstance: null,
+    currentIndex: 0,
+    volume: 1.0,
+    isBuffering: false,
   }
-}
 
- render(){
-   return (
-    <View style={styles.container}>
-      <Image 
-      style={styles.albumCover}
-      source={{
-        uri: 'http://www.archive.org/download/LibrivoxCdCoverArt8/hamlet_1104.jpg'
-      }}
-      />
-      <View style={styles.controls}>
-        <TouchableOpacity style={styles.control} onPress={()=> alert('Back button')}>
-          <Ionicons name="ios-skip-backward" size={48} color="#444"/>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.control} onPress={()=> alert('Playbutton')}>
-          {this.state.isPlaying ? (
-            <Ionicons name="ios-pause" size={48} color="#444"/>
-          ):(
-            <Ionicons name="ios-play-circle" size={48} color="#444"/>
-          )}
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.control} onPress={()=> alert('Forward button')}>
-          <Ionicons name="ios-skip-forward" size={48} color="#444"/>
-        </TouchableOpacity>
+  async componentDidMount() {
+
+    try {
+      await Audio.setAudioModeAsync({
+        allowsRecordingIOS: false,
+        interruptionModeIOS: Audio.INTERRUPTION_MODE_IOS_DO_NOT_MIX,
+        playsInSilentModeIOS: true,
+        interruptionModeAndroid: Audio.INTERRUPTION_MODE_ANDROID_DUCK_OTHERS,
+        shouldDuckAndroid: true,
+        staysActiveInBackground: true,
+        playThroughEarpieceAndroid: true,
+      })
+      this.loadAudio()
+    } catch (e) {
+      console.log(e)
+    }
+  }
+
+  async loadAudio() {
+    const {currentIndex, isPlaying, volume} = this.state
+
+    try {
+      const playbackInstance = new Audio.Sound()
+      const source = {
+        uri: audioBookPlaylist[currentIndex].uri
+      }
+
+      const status = {
+        shouldPlay: isPlaying,
+        volume
+      }
+    } catch (error) {
+      
+    }
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Image
+          style={styles.albumCover}
+          source={{
+            uri: 'http://www.archive.org/download/LibrivoxCdCoverArt8/hamlet_1104.jpg'
+          }}
+        />
+        <View style={styles.controls}>
+          <TouchableOpacity style={styles.control} onPress={() => alert('Back button')}>
+            <Ionicons name="ios-skip-backward" size={48} color="#444" />
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.control} onPress={() => alert('Playbutton')}>
+            {this.state.isPlaying ? (
+              <Ionicons name="ios-pause" size={48} color="#444" />
+            ) : (
+                <Ionicons name="ios-play-circle" size={48} color="#444" />
+              )}
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.control} onPress={() => alert('Forward button')}>
+            <Ionicons name="ios-skip-forward" size={48} color="#444" />
+          </TouchableOpacity>
+        </View>
       </View>
-    </View>
-  );
- }
-  
+    );
+  }
+
 }
 
 const styles = StyleSheet.create({
@@ -116,7 +134,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  albumCover:{
+  albumCover: {
     width: 250,
     height: 250
   },
